4ef10b782830978695df40e38c1d54ae8ca2826c
Sabin Constantin Lungu (2B || !2B) <sabinlungu293@gmail.com>
2020-01-26 23:34:53 +0000

    Added Java code for switching to specific category when the activities will be created.

--- app/src/main/java/com/example/weshopapplication/MainActivity.java
+++ app/src/main/java/com/example/weshopapplication/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.weshopapplication;
 
+import android.content.ActivityNotFoundException;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+// Author of Application: Sabin Constantin Lungu
+// Purpose of Class: Main Activity implements homepage functionality of application.
+// Last modified: 26 January 2020
+// Any Bugs?: N/A
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,4 +30,43 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    public boolean onOptionsItemSelected(MenuItem item) {
+        try {
+            
+            if (item == null) {
+                return false;
+            }
+
+            switch (item.getItemId()) {
+                case R.id.sportsAndOutdoorsCategory:
+                    //    Intent intent = new Intent(MainActivity.this, );
+
+                    return true;
+
+                case R.id.techCategory:
+                    //  Intent intent = new Intent(MainActivity.this, );
+
+                    return true;
+
+
+                case R.id.clothingCategory:
+
+                    return true;
+
+                case R.id.diyCategory:
+
+                    // Intent intent = new Intent(MainActivity.this, );
+
+                    return true; // Return true;
+
+                default:
+
+
+            }
+        } catch (ActivityNotFoundException exc) { // Catch exception
+            exc.printStackTrace();
+        }
+
+        return true;
+    }
 }
