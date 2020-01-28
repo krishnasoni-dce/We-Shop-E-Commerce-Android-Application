a3c97ff81b0409d341309e2a13b39781a3db05bb
Sabin Constantin Lungu (2B || !2B) <sabinlungu293@gmail.com>
2020-01-28 23:35:04 +0000

    Added more tests

--- app/src/androidTest/java/com/example/weshopapplication/MainActivityTest.java
+++ app/src/androidTest/java/com/example/weshopapplication/MainActivityTest.java
@@ -1,6 +1,7 @@
 package com.example.weshopapplication;
 
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -12,6 +13,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 // Purpose of Test: To test if the MainActivity loads
@@ -22,22 +24,39 @@ import static org.junit.Assert.assertNotNull;
 @LargeTest
 public class MainActivityTest {
 
+    // Rules created for each test
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Rule
     public ActivityTestRule<RegisterActivity> registerRule = new ActivityTestRule<>(RegisterActivity.class);
 
+    // Activities to be tested
     private MainActivity mainActivity = null;
-    
     private RegisterActivity registerActivity = null;
+    private LoginActivity loginActivity = null;
+    private SportsAndOutdoorsActivity sportsAndOutdoorsActivity = null;
+    private TechActivity techActivity = null;
+    private ClothingCategory clothingCategory = null;
+    private DIYActivity diyActivity = null;
+
+
+    private EditText usernameTest;
+    private EditText emailAddressTest;
+    private EditText passwordTest;
 
 
     @Before
     public void setUp() throws Exception {
         mainActivity = activityRule.getActivity(); // Get the activity
         registerActivity = registerRule.getActivity();
+
+        // Get the edit text fields
+        usernameTest = registerActivity.findViewById(R.id.usernameField);
+        emailAddressTest = registerActivity.findViewById(R.id.emailAddressField);
+        passwordTest = registerActivity.findViewById(R.id.passwordField);
     }
+
 
     @Test
     public void testMainActivityLauncher() { // Routine that tests if the main activity launches.
@@ -47,10 +66,18 @@ public class MainActivityTest {
         assertNotNull(view);
     }
 
-    @Test
-    public void rawUsernameInput() {
-
+    public void testPreconditions() {
+        assertNotNull(usernameTest);
+        assertNotNull(emailAddressTest);
+        assertNotNull(passwordTest);
     }
+
+    @Test
+    public void testUsernameEntry() { // Tests entries for the username
+
+        assertEquals("sabin29", usernameTest.getText().toString());
+    }
+
 
     @Test
     public void testRegisterActivityLauncher() {
