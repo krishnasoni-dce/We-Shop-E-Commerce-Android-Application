d188c2fd2f50e7d73afae5d88f8df08697138a69
Sabin Constantin Lungu (2B || !2B) <sabinlungu293@gmail.com>
2020-02-04 13:28:01 +0000

    Add files via upload

---
+++ We-Shop-E-Commerce-Android-Application-feature-implement-registerpage/app/src/androidTest/java/com/example/weshopapplication/MainActivityTest.java
@@ -0,0 +1,158 @@
+package com.example.weshopapplication;
+
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+
+// Purpose of Test: To test if the MainActivity loads
+// Author of Test: Sabin Constantin Lungu
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    // Rules created for each test
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Rule
+    public ActivityTestRule<RegisterActivity> registerRule = new ActivityTestRule<>(RegisterActivity.class);
+
+    @Rule
+    public ActivityTestRule<LoginActivity> loginActivityRule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Rule
+    public ActivityTestRule<TechActivity> techActivityActivityTestRule = new ActivityTestRule<>(TechActivity.class);
+
+    @Rule
+    public ActivityTestRule<BasketActivity> basketActivityActivityTestRule = new ActivityTestRule<>(BasketActivity.class);
+
+
+    // Activities to be tested
+    private MainActivity mainActivity = null;
+    private RegisterActivity registerActivity = null;
+    private LoginActivity loginActivity = null;
+
+    private SportsAndOutdoorsActivity sportsAndOutdoorsActivity = null;
+    private TechActivity techActivity = null;
+
+    private ClothingCategory clothingCategory = null;
+    private DIYActivity diyActivity = null;
+    private BasketActivity productsBasket = null;
+
+    // Retrieve the data from the fields
+    private EditText usernameTest;
+    private EditText emailAddressTest;
+    private EditText passwordTest;
+
+
+    @Before
+    public void setUp() throws Exception {
+        getActivities();
+        // Get the edit text fields
+        usernameTest = registerActivity.findViewById(R.id.usernameField);
+        emailAddressTest = registerActivity.findViewById(R.id.emailAddressField);
+        passwordTest = registerActivity.findViewById(R.id.passwordField);
+    }
+
+    public void getActivities() {
+        mainActivity = activityRule.getActivity(); // Get the activity
+        registerActivity = registerRule.getActivity();
+
+        loginActivity = loginActivityRule.getActivity(); // Get the login activity
+        techActivity = techActivityActivityTestRule.getActivity(); // Get the tech activity
+        productsBasket = basketActivityActivityTestRule.getActivity();
+    }
+
+
+    @Test
+    public void testMainActivityLauncher() { // Routine that tests if the main activity launches.
+        View view = mainActivity.findViewById(R.id.welcomeTxt);
+
+        assertNotNull(view);
+    }
+
+    public void testPreconditions() {
+        assertNotNull(usernameTest);
+        assertNotNull(emailAddressTest);
+        assertNotNull(passwordTest);
+    }
+
+    @Test
+    public void testUsernameEntryOne() { // 1. Tests entries for an empty username. Should not get accepted and should pass
+
+        assertEquals("", usernameTest.getText().toString());
+    }
+
+    @Test
+    public void testUsernameEntryTwo() { // 2. Tests for regex entries (SHOULD PASS AS THE VALIDATION WORKS 100%)
+        assertNotEquals("*£&$^", usernameTest.getText().toString());
+    }
+
+    @Test
+    public void testEmailAddressEntryOne() { // 1. Test one for the e-mail address
+        assertNotEquals("sabinlungu293@f", emailAddressTest.getText().toString()); // Will pass because this value is unexpected
+    }
+
+    @Test
+    public void testEmailAddressEntryTwo() {
+        assertNotEquals("oerkgoierjg09erjg0e9rjg09@yahoo.com", emailAddressTest.getText().toString());
+    }
+
+    @Test
+    public void testPasswordEntryOne() {
+        assertEquals("", passwordTest.getText().toString());
+    }
+
+    @Test
+    public void testPasswordUppercase() { // 2. Test 2 for the password, it should pass because the test should not expect sabin as it does not have an upper case character, otherwise it would fail.
+        assertNotEquals("sabin", passwordTest.getText().toString());
+    }
+
+    @Test
+    public void testRegisterActivityLauncher() {
+        View registerView = registerActivity.findViewById(R.id.registerTxt);
+        assertNotNull(registerView);
+    }
+
+    @Test
+    public void testTechActivityLauncher() { // Tests to see if the tech activity loads correctly.
+        View activityView = techActivity.findViewById(R.id.firstProductImg);
+        assertNotNull(activityView);
+    }
+
+    @Test
+    public void testLoadBasketActivity() { // Tests to see if the basket loads correctly.
+        View basketView = productsBasket.findViewById(R.id.placeOrderBtn);
+        assertNotNull(basketView);
+    }
+
+    @Test
+    public void testLoginActivityLauncher() { // Test stub that tests to see if the login activity launches
+        View loginView = loginActivity.findViewById(R.id.loginBtn); // Finds the login button
+        assertNotNull(loginView); // Check condition
+    }
+
+    @After
+    public void tearDown() { // After test
+        mainActivity = null;
+        registerActivity = null;
+        loginActivity = null;
+
+        techActivity = null;
+        productsBasket = null;
+    }
+}
---
+++ We-Shop-E-Commerce-Android-Application-feature-implement-registerpage/app/src/main/java/com/example/weshopapplication/LoginActivity.java
@@ -0,0 +1,213 @@
+package com.example.weshopapplication;
+
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.regex.Pattern;
+
+// Author of Application: Sabin Constantin Lungu
+// Purpose of Activity: To login a registered user
+// Date of last modified: 28th January 2020
+// Any Bugs?: N/A
+
+public class LoginActivity extends AppCompatActivity {
+    private TextView loginText; // The login text at the top of the application
+    private EditText emailAddressField;
+    private EditText passwordField;
+    private Button loginButton;
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth auth;
+    private Pattern regexPatterns = Pattern.compile("[$&+,:;=\\\\?@#|/'<>.^*()%!-]"); // Regex patterns
+    private final int logout_button_id = 101;
+    private boolean isAdded = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        this.emailAddressField = findViewById(R.id.emailAddressField);
+        this.passwordField = findViewById(R.id.passwordField);
+        this.loginButton = findViewById(R.id.loginBtn);
+
+        this.firebaseFirestore = FirebaseFirestore.getInstance();
+        this.auth = FirebaseAuth.getInstance();
+
+        this.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateEmailAddress();
+                validatePassword();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater categoriesMenu = getMenuInflater();
+        categoriesMenu.inflate(R.menu.homepagemenu, menu);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String errorMessage = "Error Caused By : ";
+
+        try {
+
+
+            switch (item.getItemId()) {
+
+                case R.id.sportsAndOutdoorsCategory: // If the sports and outdoors category is chosen
+
+                    // Create intent to go to sports and outdoors category
+
+                    Intent sportsIntent = new Intent(LoginActivity.this, SportsAndOutdoorsActivity.class);
+                    startActivity(sportsIntent);
+
+                case R.id.techCategory:
+
+                    Intent techCategoryIntent = new Intent(LoginActivity.this, TechActivity.class);
+                    startActivity(techCategoryIntent);
+
+                    return true;
+
+                case R.id.clothingCategory:
+
+                    Intent clothingCategory = new Intent(LoginActivity.this, ClothingCategory.class);
+                    startActivity(clothingCategory);
+
+                    return true;
+
+                case R.id.diyCategory:
+
+                    Intent diyIntent = new Intent(LoginActivity.this, DIYActivity.class);
+                    startActivity(diyIntent);
+
+                    return true;
+
+
+                default:
+
+                    return super.onOptionsItemSelected(item);
+            }
+
+        } catch (ActivityNotFoundException act) {
+            Log.d(errorMessage, act.toString());
+        }
+
+        return true;
+
+    }
+
+    private boolean validateEmailAddress() { // Routine that validates the e-mail address when logging in.
+        String emailEntry = emailAddressField.getText().toString().trim(); // Get the email address entry
+        String errorMessage = "Please re-enter E-mail Address";
+
+
+        if (!regexPatterns.matcher(emailEntry).find() && emailEntry.isEmpty()) { // If there is no @ symbol and the email field is empty
+
+            AlertDialog.Builder emailError = new AlertDialog.Builder(LoginActivity.this).setTitle("Error")
+                    .setMessage(errorMessage).setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            if (dialog != null) {
+                                dialog.dismiss();
+                            }
+                        }
+                    });
+
+            emailError.show();
+            emailAddressField.setError("E-mail must contain @ symbol");
+            emailAddressField.setText("");
+            return false;
+
+        } else {
+
+            emailAddressField.setError(null); // Set no error otherwise
+            login();
+            return true;
+        }
+    }
+
+
+    private boolean validatePassword() { // Routine that validates the password when logging in.
+
+        String passwordEntry = passwordField.getText().toString().trim(); // Get the password entry
+        String flushedString = "";
+
+        if (passwordEntry.isEmpty()) { // If the password field is left empty
+            AlertDialog.Builder passwordError = new AlertDialog.Builder(LoginActivity.this).setTitle("Password Error")
+                    .setMessage("Password should not be left empty")
+                    .setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            if (dialog != null) {
+                                dialog.dismiss();
+                            }
+                        }
+                    });
+
+            passwordError.show();
+            passwordField.setError("Password cannot be left empty");
+            passwordField.setText(flushedString);
+
+            return false;
+
+        } else {
+            passwordField.setError(null);
+            login();
+            return true;
+        }
+    }
+
+    private void login() { // Logs the user in
+        final String emailInput = emailAddressField.getText().toString();
+        String passwordInput = passwordField.getText().toString();
+
+        auth.signInWithEmailAndPassword(emailInput, passwordInput).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "You are logged in as " + emailInput, Toast.LENGTH_LONG).show();
+                    transitionToHomepage();
+
+                } else if (!task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Invalid credentials", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+
+    public void transitionToHomepage() {
+        try {
+            Intent homeIntent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(homeIntent);
+
+        } catch (ActivityNotFoundException act) {
+            Log.d("Error", act.toString());
+        }
+    }
+}
