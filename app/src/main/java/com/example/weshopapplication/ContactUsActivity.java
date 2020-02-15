3dd39525d7481083fa2a1225c8c61266d47f105d
Sabin Lungu <sabinlungu293@gmail.com>
2020-02-15 17:51:08 +0000

    Added Contact Us Feature.

---
+++ app/src/main/java/com/example/weshopapplication/ContactUsActivity.java
@@ -0,0 +1,14 @@
+package com.example.weshopapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ContactUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_us);
+    }
+}
